bd2cb935386c2095f7ba5ed5e66728cdc698f985
yzyDavid <yzyDavid@qq.com>
2017-12-06 16:45:58 +0800

    added basic codes.
    tweak is needed.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/*
+ * created by Zhenyun Yu.
+ */
+
+let gl = null;
+
+function initWebGL(canvas) {
+    let gl;
+    try {
+        gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    } catch (e) {
+        alert('get canvas context failed!');
+    }
+    if (!gl) {
+        alert('init webGL failed!');
+    }
+    return gl;
+}
+
+function start() {
+    const canvas = document.getElementById('root');
+    gl = initWebGL(canvas);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
