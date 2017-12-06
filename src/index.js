c373a769d1e0ac7088e9ebcd12c6f9fdd882ba70
yzyDavid <yzyDavid@qq.com>
2017-12-06 16:55:55 +0800

    finished test intial.

--- src/index.js
+++ src/index.js
@@ -20,8 +20,12 @@ function initWebGL(canvas) {
 function start() {
     const canvas = document.getElementById('root');
     gl = initWebGL(canvas);
+    if (!gl) {
+        return;
+    }
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    alert('init success');
 }
