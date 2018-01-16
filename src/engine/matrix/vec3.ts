6c3eb50cdae8d6eceff1ee1d1de9f089fa08f5ce
poiiiicen <poiiiicen@gmail.com>
2018-01-16 12:16:31 +0800

    Add normalize for vec3

--- src/engine/matrix/vec3.ts
+++ src/engine/matrix/vec3.ts
@@ -6,8 +6,9 @@
  */
 
 import {mat, vec, EPSILON} from './index';
+import {Vec3} from "../";
 
-/*
+/**
  * Transform with a mat4
  * means multiply with it, matrix on left side.
  *
@@ -24,5 +25,28 @@ export function transformMat4(a: vec, m: mat): vec {
     out[0] = (m[0] * x + m[4] * y + m[8] * z + m[12]) / w;
     out[1] = (m[1] * x + m[5] * y + m[9] * z + m[13]) / w;
     out[2] = (m[2] * x + m[6] * y + m[10] * z + m[14]) / w;
+    return out;
+}
+
+/**
+ * Normalize a vec3
+ *
+ * @param {vec3} out the receiving vector
+ * @param {vec3} a vector to normalize
+ * @returns {vec3} out
+ */
+export function normalize(a: vec): vec {
+    const out = new Array(3);
+    let x = a[0];
+    let y = a[1];
+    let z = a[2];
+    let len = x * x + y * y + z * z;
+    if (len > 0) {
+        //TODO: evaluate use of glm_invsqrt here?
+        len = 1 / Math.sqrt(len);
+        out[0] = a[0] * len;
+        out[1] = a[1] * len;
+        out[2] = a[2] * len;
+    }
     return out;
 }
