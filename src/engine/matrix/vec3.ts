3dd40d4a11741d31252456a5ac6aa7e56bac18fe
poiiiicen <poiiiicen@gmail.com>
2018-01-16 12:34:22 +0800

    Add cross

--- src/engine/matrix/vec3.ts
+++ src/engine/matrix/vec3.ts
@@ -31,9 +31,8 @@ export function transformMat4(a: vec, m: mat): vec {
 /**
  * Normalize a vec3
  *
- * @param {vec3} out the receiving vector
  * @param {vec3} a vector to normalize
- * @returns {vec3} out
+ * @returns {vec3}
  */
 export function normalize(a: vec): vec {
     const out = new Array(3);
@@ -42,11 +41,28 @@ export function normalize(a: vec): vec {
     let z = a[2];
     let len = x * x + y * y + z * z;
     if (len > 0) {
-        //TODO: evaluate use of glm_invsqrt here?
         len = 1 / Math.sqrt(len);
         out[0] = a[0] * len;
         out[1] = a[1] * len;
         out[2] = a[2] * len;
     }
+    return out;
+}
+
+/**
+ * Computes the cross product of two vec3's
+ *
+ * @param {vec3} a the first operand
+ * @param {vec3} b the second operand
+ * @returns {vec3}
+ */
+export function cross(a: vec, b: vec): vec {
+    const out = new Array(3);
+    let ax = a[0], ay = a[1], az = a[2];
+    let bx = b[0], by = b[1], bz = b[2];
+
+    out[0] = ay * bz - az * by;
+    out[1] = az * bx - ax * bz;
+    out[2] = ax * by - ay * bx;
     return out;
 }
