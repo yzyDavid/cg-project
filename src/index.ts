d147a7375854b18c4d5ecc53dc262826b8a979a4
poiiiicen <poiiiicen@gmail.com>
2018-01-16 16:22:05 +0800

    Fix " to '

--- src/index.ts
+++ src/index.ts
@@ -269,9 +269,9 @@ timeController.addListener('cameraMove', () => {
     camera.setPosition(pos);
 });
 
-mouseController.addListener("move", "mousemove", (e: MouseEvent) => {
-    console.log("x", e.clientX);
-    console.log("y", e.clientY);
+mouseController.addListener('move', 'mousemove', (e: MouseEvent) => {
+    console.log('x', e.clientX);
+    console.log('y', e.clientY);
 });
 
 camera.setEnterListener((c: Collider, info: Vec3) => {
